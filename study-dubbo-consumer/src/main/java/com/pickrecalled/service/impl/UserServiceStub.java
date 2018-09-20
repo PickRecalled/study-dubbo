271e3910f78b1748a0bac4f8ef85c0b772d58058
pickrecalled <pickrecalled@163.com>
2018-09-20 17:23:29 +0800

    本地存根

---
+++ study-dubbo-consumer/src/main/java/com/pickrecalled/service/impl/UserServiceStub.java
@@ -0,0 +1,26 @@
+package com.pickrecalled.service.impl;
+
+import com.pickrecalled.model.UserAddress;
+import com.pickrecalled.service.UserService;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+public class UserServiceStub implements UserService {
+
+	private final UserService userService;
+
+	public UserServiceStub(UserService userService) {
+		this.userService = userService;
+	}
+
+	@Override
+	public List<UserAddress> getUserAddressList(String userId) {
+		System.out.println("---UserServiceStub---");
+		if (!StringUtils.isEmpty(userId)) {
+			return userService.getUserAddressList(userId);
+
+		}
+		return null;
+	}
+}
